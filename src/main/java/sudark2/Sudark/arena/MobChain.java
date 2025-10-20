6901eade819f3e5050135649d6479c9224349437
SUDARK <2963502563@qq.com>
2025-10-20 13:32:12 +0800

    Sudark

--- src/main/java/sudark2/Sudark/arena/MobChain.java
+++ src/main/java/sudark2/Sudark/arena/MobChain.java
@@ -1,9 +1,24 @@
 package sudark2.Sudark.arena;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.List;
+import java.util.Random;
+
 import static sudark2.Sudark.arena.Arena.get;
+import static sudark2.Sudark.arena.MobContainer.mobs;
+import static sudark2.Sudark.arena.MobContainer.monsters;
 
 public class MobChain {
 
@@ -18,26 +33,56 @@ public class MobChain {
 
                 if (!pl.isOnline()) cancel();
 
+                //AFK
                 if (pl.getPitch() == oldPitch) {
-
                 } else {
                     oldPitch = pl.getPitch();
-                    trySpawn(pl);
                 }
 
+                trySpawn(pl);
                 consumeLvl(pl);
 
             }
-        }.runTaskTimerAsynchronously(get(), 0, 20);
+        }.runTaskTimerAsynchronously(get(), 0, 100);
 
     }
 
     private static void consumeLvl(Player pl) {
         pl.giveExp(-pl.getExpToLevel() / 10);
-
+        if (pl.getLevel() < 1)
+            pl.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 20, 1, false, false, false), true);
     }
 
-    private static void trySpawn(Player pl) {
+    private static void trySpawn(Player pl, int stage) {
+        Location base = pl.getLocation();
+        Random r = new Random();
 
+        float yaw = base.getYaw() + (r.nextBoolean() ? 180 : 0) + (r.nextFloat() * 60 - 30);
+        double rad = Math.toRadians(yaw);
+        double dist = 6 + r.nextDouble() * 2; // 距离 6~8 格
+
+        double dx = -Math.sin(rad) * dist;
+        double dz = Math.cos(rad) * dist;
+        Location loc = base.clone().add(dx, -1, dz);
+
+        while (loc.getBlock().getType() == Material.AIR && loc.getY() < 128) {
+            loc.add(0, 1, 0);
+        }
+
+        Mob mob = (Mob) loc.getWorld().spawnEntity(loc,mobs[stage][r.nextInt(mobs[stage].length)]);
+        monsters.addEntity(mob);
     }
+
+    public static ItemStack enchantItem(Material material, List<Enchantment> enchants, int lvl) {
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return item;
+
+        for (Enchantment enchant : enchants) {
+            meta.addEnchant(enchant, lvl, true);
+        }
+        item.setItemMeta(meta);
+        return item;
+    }
+
 }
---
+++ src/main/java/sudark2/Sudark/arena/MobContainer.java
@@ -0,0 +1,44 @@
+package sudark2.Sudark.arena;
+
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Mob;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class MobContainer {
+
+    static Team monsters;
+
+    static {
+        Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
+        monsters = board.getTeam("monsters");
+        if (monsters == null) {
+            monsters = board.registerNewTeam("monsters");
+        }
+        monsters.color(NamedTextColor.BLACK);
+        monsters.setAllowFriendlyFire(false);
+    }
+
+    static EntityType[] easy = {
+            EntityType.ZOMBIE, EntityType.SKELETON, EntityType.DROWNED
+    };
+    static EntityType[] normal = {
+            EntityType.WITHER_SKELETON, EntityType.WITCH, EntityType.BOGGED, EntityType.SPIDER
+    };
+    static EntityType[] hard = {
+            EntityType.VINDICATOR, EntityType.PHANTOM, EntityType.PIGLIN_BRUTE, EntityType.POLAR_BEAR
+    };
+    static EntityType[] extreme = {
+            EntityType.RAVAGER, EntityType.IRON_GOLEM, EntityType.ELDER_GUARDIAN, EntityType.STRAY
+    };
+    static EntityType[] boss = {
+            EntityType.WARDEN, EntityType.WITHER
+    };
+
+    static EntityType[][] mobs = {easy, normal, hard, extreme, boss};
+
+}
